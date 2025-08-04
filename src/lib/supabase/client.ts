9dcef20621052764a37957cd13520ca6a375cb57
toxac <derocon@gmail.com>
2025-08-04 22:32:09 +0530

    browser client

---
+++ src/lib/supabase/client.ts
@@ -0,0 +1,9 @@
+
+import { createBrowserClient } from '@supabase/ssr'
+
+export const supabase = createBrowserClient(
+  import.meta.env.PUBLIC_SUPABASE_URL,
+  import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+)
+
+export default supabase
